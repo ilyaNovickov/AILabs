e346fe30be3b575eb31f3bf6faea14ee35afa15a
Ilya Novikov <ilyanovickov@mail.ru>
2025-03-07 00:22:54 +0300

    ю

--- AIModel/LabDo.cs
+++ AIModel/LabDo.cs
@@ -27,42 +27,25 @@ namespace AIModel
 
         public static void foo()
         {
-            double E2 = CrossEntropia(1, Matrix<double>.Build.DenseOfArray(new double[,] { { 0.1, 0.7, 0.2 } }));
+            //double E2 = CrossEntropia(1, Matrix<double>.Build.DenseOfArray(new double[,] { { 0.1, 0.7, 0.2 } }));
 
-
+            #region chooseFuncs
             Func<double, double> activationFunc = Relu;//функция активации
             Func<double, double> divActivationFunc = DivRelu;//Производная функции активации
+            #endregion
 
             double learningRate = 0.001d;
 
 
             IEnumerable<string[]> data =  CSVHelper.ReadCSV(FileCSV);
 
-            string[] row = data.ElementAt<string[]>(0);
-
-            double trueVal = double.Parse(row[0]);//истинное значение картинки
-
-            //Значения пикселей картинки
-            List<double> vals = (from val in row select double.Parse(val)/255d).ToList<double>();
-            vals.RemoveAt(0);
-
-            Matrix<double> inputX = Vector<double>.Build.DenseOfEnumerable(vals).ToRowMatrix();//Входной вектор X (28*28=784)
-
-            /*Слой №1*/
+            #region layersValues
             int n = 10, firstLay = 10;//кол-во нейронов в слое №1
 
-            Matrix<double> w1 = DenseMatrix.Build.Random(inputX.ColumnCount, n, new ContinuousUniform(-1d, +1d));//матрица весов W
+            Matrix<double> w1 = DenseMatrix.Build.Random(28*28, n, new ContinuousUniform(-1d, +1d));//матрица весов W
 
             Matrix<double> b1 = Vector<double>.Build.Random(n, new ContinuousUniform(-1d, +1d)).ToRowMatrix();//матрица смещений b
 
-            Matrix<double> t1 = inputX * w1 + b1;//вектор t
-
-            //Matrix<double> h1 = Matrix.Build.Dense(1, n, (row, col) => activationFunc(t1, col));//Заполнение значений слоя h
-
-            Matrix<double> h1 = t1.Map(activationFunc);
-            /*КОНЕЦ Слой №1*/
-
-            /*Слой №2*/
             int seclayer = 15;//кол-во нейронов в слое №1
             n = 15;
 
@@ -70,48 +53,96 @@ namespace AIModel
 
             Matrix<double> b2 = Vector<double>.Build.Random(n, new ContinuousUniform(-1d, +1d)).ToRowMatrix();//матрица смещений b
 
-            Matrix<double> t2 = h1 * w2 + b2;//вектор t
-            Matrix<double> h2 =  t2.Map(activationFunc);
-            //Matrix<double> h2 = Matrix.Build.Dense(1, n, (row, col) => activationFunc(t1, col));//Заполнение значений слоя h
-            /*КОНЕЦ Слой №2*/
-
-            /*Слой №3*/
             int thirdLayer = 10;//кол-во нейронов в слое №1
             n = 10;
 
             Matrix<double> w3 = DenseMatrix.Build.Random(w2.ColumnCount, n, new ContinuousUniform(-1d, +1d));//матрица весов W
 
             Matrix<double> b3 = Vector<double>.Build.Random(n, new ContinuousUniform(-1d, +1d)).ToRowMatrix();//матрица смещений b
+            #endregion
 
-            Matrix<double> t3 = h2 * w3 + b3;//вектор t
+            for (int epoch = 0; epoch < 10; epoch++)
+            {
+                for (int i = 0; i < data.Count(); i++)
+                {
+                    #region getData
+                    string[] row = data.ElementAt<string[]>(i);
 
-            //Matrix<double> h3 = Matrix.Build.Dense(1, n, (row, col) => activationFunc(t1, col));//Заполнение значений слоя h
-            /*КОНЕЦ Слой №3*/
+                    double trueVal = double.Parse(row[0]);//истинное значение картинки
 
-            Matrix<double> z = SoftMax(t3);
-            double E = CrossEntropia(trueVal, z);
+                    //Значения пикселей картинки
+                    List<double> vals = (from val in row select double.Parse(val) / 255d).ToList<double>();
+                    vals.RemoveAt(0);
+                    #endregion
+                    #region calculate
+                    Matrix<double> inputX = Vector<double>.Build.DenseOfEnumerable(vals).ToRowMatrix();//Входной вектор X (28*28=784)
 
-            //Обучение
-            Matrix<double> trueY = ValueToMatrix(trueVal, 10);
+                    /*Слой №1*/
+                    w1 = DenseMatrix.Build.Random(inputX.ColumnCount, n, new ContinuousUniform(-1d, +1d));//матрица весов W
 
-            var dE_dt3 = z - trueY;
-            //var dE_dw3 = h3
+                    b1 = Vector<double>.Build.Random(n, new ContinuousUniform(-1d, +1d)).ToRowMatrix();//матрица смещений b
 
-            //Matrix<double> dE_dh1 = 0;
-            //Matrix<double> dE_dt1 = dE_dh1.PointwiseMultiply(Vector<double>.Build.Dense(t1.ColumnCount, (colum) => divActivationFunc(t1, colum)).ToRowMatrix());
+                    Matrix<double> t1 = inputX * w1 + b1;//вектор t
 
-            //# обратное распространение 
-            //            y_vect = to_prob_vect(y, OUT_DIM)
-            //                dE_dt2 = z - y_vect
-            //                dE_dW2 = h1.T @ dE_dt2
-            //                dE_db2 = dE_dt2
-            //                dE_dh1 = dE_dt2 @ W2.T
-            //                dE_dt1 = dE_dh1 * relu_deriv(t1)
-            //                dE_dW1 = x.T @ dE_dt1
-            //                dE_db1 = dE_dt1
+                    //Matrix<double> h1 = Matrix.Build.Dense(1, n, (row, col) => activationFunc(t1, col));//Заполнение значений слоя h
 
-            Matrix<double> test = Matrix<double>.Build.DenseOfArray(new double[,] { { 10, 5, 3 } }).PointwiseMultiply(Matrix<double>.Build.DenseOfArray(new double[,] { { 1, 2, 5} }));
-            //Matrix<double> test2 = Matrix<double>.Build.DenseOfArray(new double[,] { { 10, 5, 3 } }).Multiply(Matrix<double>.Build.DenseOfArray(new double[,] { { 1, 2, 5 } }));
+                    Matrix<double> h1 = t1.Map(activationFunc);
+                    /*КОНЕЦ Слой №1*/
+
+                    /*Слой №2*/
+                    w2 = DenseMatrix.Build.Random(w1.ColumnCount, n, new ContinuousUniform(-1d, +1d));//матрица весов W
+
+                    b2 = Vector<double>.Build.Random(n, new ContinuousUniform(-1d, +1d)).ToRowMatrix();//матрица смещений b
+
+                    Matrix<double> t2 = h1 * w2 + b2;//вектор t
+                    Matrix<double> h2 = t2.Map(activationFunc);
+                    //Matrix<double> h2 = Matrix.Build.Dense(1, n, (row, col) => activationFunc(t1, col));//Заполнение значений слоя h
+                    /*КОНЕЦ Слой №2*/
+
+                    /*Слой №3*/
+                    w3 = DenseMatrix.Build.Random(w2.ColumnCount, n, new ContinuousUniform(-1d, +1d));//матрица весов W
+
+                    b3 = Vector<double>.Build.Random(n, new ContinuousUniform(-1d, +1d)).ToRowMatrix();//матрица смещений b
+
+                    Matrix<double> t3 = h2 * w3 + b3;//вектор t
+
+                    //Matrix<double> h3 = Matrix.Build.Dense(1, n, (row, col) => activationFunc(t1, col));//Заполнение значений слоя h
+                    /*КОНЕЦ Слой №3*/
+                    #endregion
+                    #region learning
+                    Matrix<double> z = SoftMax(t3);
+                    double E = CrossEntropia(trueVal, z);
+
+                    //Обучение
+                    Matrix<double> trueY = ValueToMatrix(trueVal, 10);
+
+                    Matrix<double> dE_dt3 = z - trueY;
+                    Matrix<double> dE_dw3 = h2.Transpose() * dE_dt3;
+                    Matrix<double> dE_db3 = dE_dt3.Clone();
+
+                    Matrix<double> dE_dh2 = dE_dt3 * w3.Transpose();
+                    Matrix<double> dE_dt2 = dE_dh2.PointwiseMultiply(Vector<double>.Build.Dense(t2.ColumnCount,
+                        (colum) => divActivationFunc(t2.At(0, colum))).ToRowMatrix());
+                    Matrix<double> dE_dw2 = h1.Transpose() * dE_dt2;
+                    Matrix<double> dE_db2 = dE_dt2.Clone();
+
+                    Matrix<double> dE_dh1 = dE_dt2 * w2.Transpose();
+                    Matrix<double> dE_dt1 = dE_dh1.PointwiseMultiply(Vector<double>.Build.Dense(t1.ColumnCount,
+                        (colum) => divActivationFunc(t1.At(0, colum))).ToRowMatrix());
+                    Matrix<double> dE_dw1 = inputX.Transpose() * dE_dt1;
+                    Matrix<double> dE_db1 = dE_dt1.Clone();
+
+                    w3 = w3 - learningRate * dE_dw3;
+                    b3 = b3 - learningRate * dE_db3;
+                    w2 = w2 - learningRate * dE_dw2;
+                    b2 = b2 - learningRate * dE_db2;
+                    w1 = w1 - learningRate * dE_dw1;
+                    b1 = b1 - learningRate * dE_db1;
+                    #endregion
+                }
+            }
+            
+
         }
 
         //[Obsolete]
